ed7aa442f911c64ae450165394be7c3b120907a8
Wang Bei Jing <2231101379@qq.com>
2020-12-21 15:33:00 +0800

    v1.0.1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  {{msg}}
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent ({
+  name: 'App',
+  data(){
+    return {
+      msg:"hello world"
+    } 
+  },
+  components: {
+    HelloWorld
+  }
+})
+</script>
