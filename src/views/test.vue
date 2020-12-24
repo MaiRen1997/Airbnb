532bce78de3739db7e3b3a7873926dbded69c08e
Hu weiqiaing <1149707097@qq.com>
2020-12-24 10:10:07 +0800

    img

---
+++ src/views/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item v-for="(item, index) in 4" :key="index">1</van-swipe-item>
+    </van-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      img:[{
+        title:11,
+        url:"../assets/img/lunbo1.jpg"
+      },{
+        title:22,
+        url:"../assets/img/lunbo1.jpg"
+      }]
+    }
+  }
+}
+</script>
